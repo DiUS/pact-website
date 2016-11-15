c378ce0450eca8c80c8cf415fa371dad3ad8b623
Jun Zheng <arthurjordan@gmail.com>
2016-11-15 21:21:46 +1100

    WIP registration form

--- js/main.js
+++ js/main.js
@@ -215,3 +215,41 @@ $(document).ready(function() {
   //Placeholder Plugin Settings
   $('input, textarea, select, email, password').placeholder();
 });
+
+
+$(document).ready(function() {
+  var URL = 'https://z4bzdlc927.execute-api.us-east-1.amazonaws.com/staging/register'
+
+  $('#register-form').submit(function(event){
+   event.preventDefault();
+
+   var data = {
+     name: $("#first_name").val(),
+     email: $('#email').val(),
+     url: $('#company_url').val()
+   }
+
+   console.log(name, email, url);
+
+   $.ajax({
+     type: 'POST',
+     url: URL,
+     dataType: 'json',
+     contentType: 'application/json',
+     data: JSON.stringify(data),
+     success: function(){
+       alert("clear form and show a success message");
+       console.log("clear form and show a success message");
+     },
+     error: function(){
+       alert("ooops, sth went wrong");
+       console.log("ooops, sth went wrong")
+     };
+   })
+
+  // $('#submit-button').click(function(){ alert(" submit button clicked 17:14" )});
+  // console.log("inside docment.ready function");
+  // })
+
+  });
+});
