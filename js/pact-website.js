9d452a6be1bf824ce59674da7aefe36bfe7a4b11
Jun Zheng <arthurjordan@gmail.com>
2016-11-16 10:40:11 +1100

    register form and lambda working

--- js/pact-website.js
+++ js/pact-website.js
@@ -1,39 +1,33 @@
-$(document).ready(function() {
-	// var URL = 'https://z4bzdlc927.execute-api.us-east-1.amazonaws.com/staging/register'
+$(document).ready(function(){
+  console.log("works");
 
-	// $('#register-form').submit(function(event){
-	// 	event.preventDefault();
-
-	// 	var data = {
-	// 		name: $("#first_name").val(),
-	// 		email: $('#email').val(),
-	// 		url: $('#company_url').val()
-	// 	}
-
-	// 	console.log(name, email, url);
-
-	// 	$.ajax({
-	// 		type: 'POST',
-	// 		url: URL,
-	// 		dataType: 'json',
-	// 		contentType: 'application/json',
-	// 		data: JSON.stringify(data),
-	// 		success: function(){
-	// 			alert("clear form and show a success message");
-	// 			console.log("clear form and show a success message");
-	// 		},
-	// 		error: function(){
-	// 			alert("ooops, sth went wrong");
-	// 			console.log("ooops, sth went wrong")
-	// 		}
-	// 	})
-
-	$('#submit-button').click(function(){ alert(" submit button clicked 17:08" )});
-	console.log("inside docment.ready function");
-	})
-
-
-
-
+  var URL = 'https://z4bzdlc927.execute-api.us-east-1.amazonaws.com/staging/register'
+ 
+$('#register-form').submit(function (event) {
+  event.preventDefault();
+ 
+  var data = {
+    company_name: $("#company_name").val(),
+    first_name: $("#first_name").val(),
+    last_name: $("#last_name").val(),
+    company_url: $('#company_url').val(),
+    email: $('#email').val()
+  }
+ 
+  $.ajax({
+    type: 'POST',
+    url: URL,
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function () {
+      console.log("success, now clear form values");
+      $('input').val("");
+    },
+    error: function () {
+      console.log("ooops, failed");
+    }
+  })
+})
 
 });
