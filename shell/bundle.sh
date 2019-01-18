f789104f6f66699879c5af60d7275b43c2d0111b
Beth Skurrie <beth@bethesque.com>
2019-01-18 14:18:42 +1100

    fix: correct bundle.sh script

--- shell/bundle.sh
+++ shell/bundle.sh
@@ -3,11 +3,12 @@
 set -e
 
 docker run -v $(pwd)/.:/app node:8.1-alpine  /bin/ash -c '
+apk update && apk upgrade && apk add --no-cache bash git openssh
 cd /app
 # javascript stuff
 npm i grunt-cli
 npm i
-bower install
+./node_modules/bower/bin/bower install --allow-root
 
 # bundle javascript assets
 ./node_modules/grunt-cli/bin/grunt bundle
