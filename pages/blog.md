39b8d5922f40f5c8ab6cd53c6d70085f061cfa08
Matt Fellows <matt.fellows@onegeek.com.au>
2019-04-25 12:08:34 +1000

    wip: initial push of new website

---
+++ pages/blog.md
@@ -0,0 +1,11 @@
+---
+layout: blog
+title: Blog Test
+permalink: '/blogtest/'
+author:  Matt Fellows
+date: 2019-04-23
+---
+
+# IS this a heading? What happens?
+
+This is a sentence, how does it mark up?
--- shell/deploy.sh
+++ shell/deploy.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ "${ENVIRONMENT}" == "dev" ]; then
+  AWS_S3_BUCKET="s3://staging.pactflow.io"
+else
+  AWS_S3_BUCKET="s3://pactflow.io"
+fi
+
 aws s3 sync ./public/ $AWS_S3_BUCKET \
   --acl public-read \
   --cache-control "max-age=1800, s-max-age=1800, must-revalidate" \
