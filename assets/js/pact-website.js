e7c7822d919469a4610d4cf625ef48570e616915
Matt Fellows <matt.fellows@onegeek.com.au>
2019-05-01 15:00:48 +1000

    feat(subscriptions): chargebee integration

--- assets/js/pact-website.js
+++ assets/js/pact-website.js
@@ -1,22 +1,20 @@
 $(document).ready(function(){
 
   var urlParams = new URLSearchParams(window.location.search);
-  var REGISTRATIONS_GATEWAY_URL_DEV = 'https://2c6kjv2409.execute-api.ap-southeast-2.amazonaws.com/Stage/api/registrations'
-  var REGISTRATIONS_GATEWAY_URL_PROD = 'https://h3dk1l7b62.execute-api.ap-southeast-2.amazonaws.com/Prod/api/registrations'
 
   // Chargebee configurations
-  // Staging: https://staging.pactflow.io/register/complete/?sub_id={{subscription.id}}&invoice_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}/register/complete/?sub_id={{subscription.id}}&invoice_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}
-  // Prod: https://pactflow.io/register/complete/?sub_id={{subscription.id}}&invoice_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}/register/complete/?sub_id={{subscription.id}}&invoice_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}
-  // Local dev: http://localhost:4000/register/complete/?sub_id={{subscription.id}}&invoice_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}/register/complete/?sub_id={{subscription.id}}&invoice_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}
-  // http://localhost:4000/register/complete/?sub_id=1234&invoice_id=5678&plan_id=team5&first_name=Matt&last_name=Fellows&email=m@onegeek.com.ua&company=test%20company
+  // Staging: https://staging.pactflow.io/register/complete/?sub_id={{subscription.id}}&customer_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}/register/complete/?sub_id={{subscription.id}}&customer_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}
+  // Prod: https://pactflow.io/register/complete/?sub_id={{subscription.id}}&customer_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}/register/complete/?sub_id={{subscription.id}}&customer_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}
+  // Local dev: http://localhost:4000/register/complete/?sub_id={{subscription.id}}&customer_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}/register/complete/?sub_id={{subscription.id}}&customer_id={{invoice.id}}&plan_id={{plan.id}}&first_name={{customer.first_name}}&last_name={{customer.last_name}}&email={{customer.email}}&company={{customer.company}}
+  // http://localhost:4000/register/complete/?sub_id=1234&customer_id=5678&plan_id=team5&first_name=Matt&last_name=Fellows&email=m@onegeek.com.ua&company=test%20company
 
   // If on the complete signup page, pre-fill the form
   if (window.location.pathname === "/register/complete/") {
     if (urlParams.has('sub_id')) {
       $("#subscription_id").val(urlParams.get('sub_id'))
     }
-    if (urlParams.has('invoice_id')) {
-      $("#invoice_id").val(urlParams.get('invoice_id'))
+    if (urlParams.has('customer_id')) {
+      $("#customer_id").val(urlParams.get('customer_id'))
     }
     if (urlParams.has('plan_id')) {
       $("#plan_id").val(urlParams.get('plan_id'))
@@ -36,76 +34,80 @@ $(document).ready(function(){
   }
 
 
-$('#register-form').submit(function (event) {
-  event.preventDefault();
+  $('#register-form').submit(function (event) {
+    event.preventDefault();
 
-  var data = {
-    requested_subdomain: $("#requested_subdomain").val(),
-    company_name: $("#company_name").val(),
-    first_name: $("#first_name").val(),
-    last_name: $("#last_name").val(),
-    company_url: $('#company_url').val(),
-    email: $('#email').val(),
-    how_hear: $('#how_hear').val(),
-    plan_id: $('#plan_id').val(),
-    subscription_id: $('#subscription_id').val(),
-    invoice_id: $('#invoice_id').val(),
-    github_auth: $('#github_auth').prop('checked')
-  }
+    var data = {
+      requested_subdomain: $("#requested_subdomain").val(),
+      company_name: $("#company_name").val(),
+      first_name: $("#first_name").val(),
+      last_name: $("#last_name").val(),
+      company_url: $('#company_url').val(),
+      email: $('#email').val(),
+      how_hear: $('#how_hear').val(),
+      plan_id: $('#plan_id').val(),
+      subscription_id: $('#subscription_id').val(),
+      customer_id: $('#customer_id').val(),
+      github_auth: $('#github_auth').prop('checked'),
+      cognito_enabled: $('#cognito_enabled').prop('checked')
+    }
 
-  $.ajax({
-    type: 'POST',
-    url: REGISTRATIONS_GATEWAY_URL_DEV,
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(data),
-  })
+    $.ajax({
+      type: 'POST',
+      url: REGISTRATIONS_GATEWAY_URL,
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: res => {
+        console.log('response from provisioning: ', res)
+        $("#form-failure-alert").hide()
 
-  $.ajax({
-    type: 'POST',
-    url: REGISTRATIONS_GATEWAY_URL_PROD,
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(data),
-  })
+        // Do it on the actual succes!
+        console.log('done');
+        $('#register-form')[0].reset();
+        $('.registration').hide();
+        $('.signup-complete').show();
+      },
+      error: e => {
+        console.log('error provisioning:', e)
+        $("#form-failure-alert").html("There was an error creating your account, please try again or contact support@pactflow.io for assistance");
+        $("#form-failure-alert").fadeTo(2000, 200);
+        $("#form-failure-alert-bottom").html("There was an error creating your account, please try again or contact support@pactflow.io for assistance");
+        $("#form-failure-alert-bottom").fadeTo(2000, 200);
+      },
+      complete: (a,b,c) => {
+        console.log('completed?', a, b, c)
+      }
+    })
 
-  try {
-    // Registery event with GA
-    ga('send', 'event', {
-      eventCategory: 'registration',
-      eventAction: 'complete',
-      eventLabel: 'beta'
-    });
-  } catch (e) {
-    console.log("Exception reporting to GA: " + e);
-  }
-})
-
-$('#requested_subdomain').on('blur', () => {
-  $.ajax({
-    type: 'GET',
-    url: REGISTRATIONS_GATEWAY_URL_DEV + '/subdomain/' + $('#requested_subdomain').val(),
-    dataType: 'json',
-  }).then((res) => {
-    console.log('response:', res)
-    if (res.available == false) {
-      $(".alert").slideUp(500);
-      $("#failure-alert").html("Hosted subdomain '" + $('#requested_subdomain').val() + " 'is unavailable, please try another");
-      $("#failure-alert").fadeTo(2000, 200);
-      $('#requested_subdomain').val('')
-    } else {
-      $("#failure-alert").hide()
+    try {
+      // Registery event with GA
+      ga('send', 'event', {
+        eventCategory: 'registration',
+        eventAction: 'complete',
+        eventLabel: 'beta'
+      });
+    } catch (e) {
+      console.log("Exception reporting to GA: " + e);
     }
   })
-})
 
+  $('#requested_subdomain').on('blur', () => {
+    $.ajax({
+      type: 'GET',
+      url: REGISTRATIONS_GATEWAY_URL + '/subdomain/' + $('#requested_subdomain').val(),
+      dataType: 'json',
+    }).then((res) => {
+      console.log('lookup response:', res)
 
-$('#register-form').on('submit', function(){
-  setTimeout(function () {
-    console.log('done');
-    $('#register-form')[0].reset();
-    $('.registration').hide();
-    $('.signup-complete').show();
-  }, 500);
-})
+      if (res.available == false) {
+        $(".alert").slideUp(500);
+        $("#failure-alert").html("Hosted subdomain '" + $('#requested_subdomain').val() + " 'is unavailable, please try another");
+        $("#failure-alert").fadeTo(2000, 200);
+        $('#requested_subdomain').val('')
+      } else {
+        $("#failure-alert").hide()
+      }
+    })
+  })
 });
