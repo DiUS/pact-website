3eb3c93810f6b2a7b6d9811bd8bb9e5c5f5704e2
Ken Ong <kenming.ong@railslice.com>
2019-12-10 22:32:48 +1100

    chore: deploy templates before website. align S3 source

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -4,13 +4,9 @@ set -e
 
 export ENVIRONMENT
 export JEKYLL_ENV
+export S3BUCKET_NAME
 
-if [ "${ENVIRONMENT}" == "dev" ]; then
-  AWS_S3_BUCKET="s3://staging.pactflow.io"
-else
-  AWS_S3_BUCKET="s3://pactflow.io"
-fi
-
+AWS_S3_BUCKET="s3://${S3BUCKET_NAME}"
 ./shell/build.sh
 
 aws s3 sync ./public/ $AWS_S3_BUCKET \
